3f1fcaa670b2f4367d56cd432814e95e98f51b12
bfresnel <benoit.fresnel.1@gmail.com>
2020-01-25 01:14:54 +0100

    Initial commit

---
+++ twitchBonusClicker.js
@@ -0,0 +1,15 @@
+// Script part
+setInterval(function() {
+    clickOnButtonIfBonusButtonIsAvailable();
+}, 305000);
+
+// function part
+function clickOnButtonIfBonusButtonIsAvailable(){
+    var bonusButton = document.getElementsByClassName('tw-button tw-button--success tw-interactive');
+    if (bonusButton.length === 0){
+        console.log('[TwitchButtonClicker] Bonus button is not available for the moment');
+    } else {
+        console.log('[TwitchButtonClicker] Bonus button is available !');
+        bonusButton[0].click();
+    }
+}
