f518a11dfb9787a40889bd42cbb3e17ed3ab95ee
bfresnel <benoit.fresnel.1@gmail.com>
2020-02-23 16:45:54 +0100

    [0.0.25] new logs

--- twitchBonusClicker.js
+++ twitchBonusClicker.js
@@ -1,15 +1,26 @@
 // Script part
-setInterval(function() {
+var intervalId;
+
+intervalId = setInterval(function() {
     clickOnButtonIfBonusButtonIsAvailable();
-}, 150000);
+}, 60000);
 
 // function part
 function clickOnButtonIfBonusButtonIsAvailable(){
     var bonusButton = document.getElementsByClassName('tw-button tw-button--success tw-interactive');
     if (bonusButton.length === 0){
-        console.log('[TwitchButtonClicker] Bonus button is not available for the moment');
+        console.log(displayCurrentTime() + ' - [TwitchButtonClicker] Bonus button is not available for the moment');
     } else {
-        console.log('[TwitchButtonClicker] Bonus button is available !');
+        console.log(displayCurrentTime() + ' - [TwitchButtonClicker] Bonus button is available !');
         bonusButton[0].click();
     }
+}
+
+function displayCurrentTime() {
+    var currentDate = new Date();
+    var hour = currentDate.getHours();
+    var minute = currentDate.getMinutes();
+    var seconde = currentDate.getSeconds();
+
+    return '[' + hour + ':' + minute + ':' + seconde + ']';
 }
