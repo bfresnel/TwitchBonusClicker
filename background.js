8f815c6c810ce41aa0dd6783beba0137d84d74aa
bfresnel <benoit.fresnel.1@gmail.com>
2020-04-09 16:35:47 +0200

    [0.0.26] Script name change + Typo when displaying current time

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ intervalId = setInterval(function() {
 function clickOnButtonIfBonusButtonIsAvailable(){
     var bonusButton = document.getElementsByClassName('tw-button tw-button--success tw-interactive');
     if (bonusButton.length === 0){
-        console.log(displayCurrentTime() + ' - [TwitchButtonClicker] Bonus button is not available for the moment');
+        console.log('[TwitchButtonClicker]' + displayCurrentTime() + ' -  Bonus button is not available for the moment');
     } else {
         console.log(displayCurrentTime() + ' - [TwitchButtonClicker] Bonus button is available !');
         bonusButton[0].click();
@@ -21,6 +21,18 @@ function displayCurrentTime() {
     var hour = currentDate.getHours();
     var minute = currentDate.getMinutes();
     var seconde = currentDate.getSeconds();
+
+    if (hour < 10){
+        hour = '0' + hour;
+    }
+
+    if (minute < 10){
+        minute = '0' + minute;
+    }
+
+    if (seconde < 10){
+        seconde = '0' + seconde;
+    }
 
     return '[' + hour + ':' + minute + ':' + seconde + ']';
 }
