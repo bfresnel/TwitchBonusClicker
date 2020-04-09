2b16f9dc21668b917bbeba4d79338db611307e82
bfresnel <benoit.fresnel.1@gmail.com>
2020-04-10 01:04:53 +0200

    [0.1.0] Still WIP

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 // Script part
 var intervalId;
 
-intervalId = setInterval(function() {
+
+var inputContainer = document.getElementsByClassName('community-points-summary tw-align-items-center tw-flex tw-full-height');
+// child : class="tw-full-height tw-relative tw-z-above"
+//child : tw-transition tw-transition--duration-long tw-transition--enter-done tw-transition__fade tw-transition__fade--enter-done
+//child : tw-transition tw-transition--duration-long tw-transition--enter-done tw-transition__slide-over-left tw-transition__slide-over-left--enter-done
+//child : tw-inline-flex tw-relative tw-tooltip-wrapper
+// child : Button : tw-button tw-button--success tw-interactive
+window.addEventListener('load', (event) => {
+    console.log(inputContainer);
+});
+
+/* intervalId = setInterval(function() {
     clickOnButtonIfBonusButtonIsAvailable();
 }, 60000);
 
@@ -14,7 +25,7 @@ function clickOnButtonIfBonusButtonIsAvailable(){
         console.log(displayCurrentTime() + ' - [TwitchButtonClicker] Bonus button is available !');
         bonusButton[0].click();
     }
-}
+} */
 
 function displayCurrentTime() {
     var currentDate = new Date();
